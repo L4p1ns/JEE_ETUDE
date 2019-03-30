b4a9315242964e81610db5df162f616b42dfeee9
bayendembaseye <bayendemba.seye@koonda.fr>
2019-03-30 11:58:26 +0000

    Initial commit.

---
+++ src/controller/HomeController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// Annotation
+@WebServlet("/home")
+public class HomeController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("/WEB-INF/pages/Home.jsp").forward(req, resp);
+    }
+}
---
+++ web/WEB-INF/pages/Home.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ADMIN
+  Date: 30/03/2019
+  Time: 11:30
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+<h4>Salut Home</h4>
+</body>
+</html>
